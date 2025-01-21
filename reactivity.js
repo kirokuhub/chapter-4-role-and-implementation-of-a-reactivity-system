65658e1d441235942948a26903bcfcd6c228bb8e
Friedrich Chen <friedrichchen@Inuyasha.local>
2025-01-21 20:11:33 +0800

    feat: decouple and extract track and trigger logic

--- reactivity.js
+++ reactivity.js
@@ -8,37 +8,44 @@ let activeEffect;
 
 export const obj = new Proxy(data, {
   get(target, key) {
-    if(!activeEffect) {
-      return;
-    }
-
-    let depsMap = bucket.get(target);
-    if(!depsMap) {
-      bucket.set(target, (depsMap = new Map()));
-    }
-
-    let deps = depsMap.get(key);
-    if(!deps) {
-      depsMap.set(key, (deps = new Set()));
-    }
-
-    deps.add(activeEffect);
-    activeEffect.deps.push(deps);
+    track(target, key);
     return target[key];
   },
   set(target, key, value) {
     target[key] = value;
-
-    const depsMap = bucket.get(target);
-    if(!depsMap) {
-      return;
-    }
-    const deps = depsMap.get(key);
-    const depsToRun = new Set(deps);
-    depsToRun.forEach((fn) => fn());
+    trigger(target, key);
     return true;
   },
 });
+
+function track(target, key) {
+  if(!activeEffect) {
+    return;
+  }
+
+  let depsMap = bucket.get(target);
+  if(!depsMap) {
+    bucket.set(target, (depsMap = new Map()));
+  }
+
+  let deps = depsMap.get(key);
+  if(!deps) {
+    depsMap.set(key, (deps = new Set()));
+  }
+
+  deps.add(activeEffect);
+  activeEffect.deps.push(deps);
+}
+
+function trigger(target, key) {
+  const depsMap = bucket.get(target);
+  if(!depsMap) {
+    return;
+  }
+  const deps = depsMap.get(key);
+  const depsToRun = new Set(deps);
+  depsToRun.forEach((fn) => fn());
+}
 
 export function effect(fn) {
   const effectFn = () => {
