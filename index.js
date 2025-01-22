4f5025188c567fc44e5709187e390a63025c44b6
Friedrich Chen <friedrichchen@Inuyasha.local>
2025-01-22 20:22:53 +0800

    feat: enabling caching capabilities

--- index.js
+++ index.js
@@ -4,4 +4,10 @@ const { log } = console;
 const sum = computed(() => obj.foo + obj.bar);
 setTimeout(() => {
   log('sum.value: ', sum.value);
+
+  setTimeout(() => {
+    obj.foo++;
+    log('sum.value - 2: ', sum.value);
+  }, 2000);
+  log('\n');
 }, 2000);
--- reactivity.js
+++ reactivity.js
@@ -101,12 +101,23 @@ export function flushJob() {
 }
 
 export function computed(getter) {
+  let value;
+  let dirty = true;
   const effectFn = effect(getter, {
     lazy: true,
+    scheduler() {
+      if(!dirty) {
+        dirty = true;
+      }
+    }
   });
   const obj = {
     get value() {
-      return effectFn();
+      if(dirty) {
+        value = effectFn();
+        dirty = false;
+      }
+      return value;
     }
   }
   return obj;
